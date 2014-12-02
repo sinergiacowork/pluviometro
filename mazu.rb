7643e51c3faa52ad223db36a6b907101a6ddd78c
elCuervo <yo@brunoaguirre.com>
2014-12-01 23:52:58 -0200

    Show last event

--- mazu.rb
+++ mazu.rb
@@ -1,17 +1,20 @@
 require 'ruby_spark'
 require 'cuba'
+require 'json'
 
 Dir['./models/*.rb'].each  { |f| require f }
 Dir['./workers/*.rb'].each { |f| require f }
 
 Shoden.setup
-
-SuckerPunch.logger = Logger.new('sucker_punch.log')
 MeasureRainJob.new.async.perform
 
 Cuba.define do
   on root do
     rain = Rain.last
-    res.write rain.mm if !rain.nil?
+
+    if !rain.nil?
+      res['Content-Type'] = 'application/json'
+      res.write JSON.dump({ timestamp: rain.created_at, mm: rain.mm })
+    end
   end
 end
