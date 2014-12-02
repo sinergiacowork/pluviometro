d912ba60b4290abff12ac0db3934e29b736a685a
elCuervo <yo@brunoaguirre.com>
2014-12-02 00:06:16 -0200

    Show default out

--- mazu.rb
+++ mazu.rb
@@ -12,9 +12,13 @@ Cuba.define do
   on root do
     rain = Rain.last
 
-    if !rain.nil?
-      res['Content-Type'] = 'application/json'
-      res.write JSON.dump({ timestamp: rain.created_at, mm: rain.mm })
+    out =  if !rain.nil?
+      { timestamp: rain.created_at, mm: rain.mm }
+    else
+      {}
     end
+
+   res['Content-Type'] = 'application/json'
+   res.write JSON.dump(out)
   end
 end
